1439024c4389d21bfaa6b48411d9d227332407fe
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-06-19 18:55:49 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
 
 
+/*
+
 class Film{
     #name;
     #author;
@@ -28,3 +30,30 @@ class Film{
 
 const film = new Film("Avatar", "Cameron", 240);
 console.log(film);
+
+*/
+
+const Book = function(title, author){
+    this.title = title;
+    this.author = author;
+
+}
+
+Book.prototype.buy = function(){
+    console.log("Buy");
+}
+
+const AudioBook = function(title, author, lengthMin){
+    Book.call(this, title, author);
+    this.lengthMin = lengthMin;
+}
+
+AudioBook.prototype = Object.create(Book.prototype);
+
+AudioBook.prototype.log = function(){
+    console.log(`${this.title} - ${this.lengthMin}`);
+}
+
+const book = new AudioBook("Lotr", "Tolkien", 20*60);
+book.log();
+book.buy();
