6b140a8f6da55e5a50a4d194e7e3309db6f0692d
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-06-20 14:57:33 +0300

    Add files via upload

--- script2.js
+++ script2.js
@@ -29,3 +29,47 @@ class EBook extends Book {
 
 const book2 = new EBook("LOTR", "Tolkien", 100);
 book2.info();
+
+//======= Builder Pattern and Chaining ====================
+
+class Wallet {
+    balance = 0;
+
+    add(sum){
+        this.balance += sum;
+        return this;
+    }
+
+    remove(sum){
+        this.balance -= sum;
+        return this;
+    }
+}
+
+const wallet = new Wallet();
+const res = wallet
+        .add(100)
+        .remove(10)
+console.log(res);
+
+
+class Builder {
+        house = {};
+
+        addRoof(){
+            this.house.roof = "Roof";
+            return this;
+        }
+
+        addFloor(){
+            this.house.floor = "Floor";
+            return this;
+        }
+
+        execute(){
+            return this.house;
+        }
+}
+
+const result = new Builder().addRoof().addFloor().execute();
+console.log(result);
